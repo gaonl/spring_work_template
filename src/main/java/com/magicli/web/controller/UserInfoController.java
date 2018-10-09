2f57861029ca7c5e29897d45596f0c482dec0796
gaonl <gnlyjy@163.com>
2018-10-09 14:16:39 +0800

    commit

--- src/main/java/com/magicli/web/controller/UserInfoController.java
+++ src/main/java/com/magicli/web/controller/UserInfoController.java
@@ -5,12 +5,19 @@ import com.magicli.ioc.domain.User;
 import com.magicli.web.controller.exceptions.DuplicateDomainException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by gaonl on 2018/10/6.
@@ -35,6 +42,68 @@ public class UserInfoController {
     }
 
     /**
+     * restful api
+     * produces = "application/json" 表明这个方法只产生json数据，即发送请求时，声明Accept头部要么不设置（说明可以接受任意类型），要么为[Accept: application/json]，其他调用此方法将报错
+     * 标注@ResponseBody 表明这个方法的返回值是http resopnse body，并启用相应的Converter将返回值转换成相应的数据形式，入json
+     * 当返回ResponseEntity<T>的时候，可以不用标注@ResponseBody，功能同上，可以添加响应状态码，头部信息等等
+     *
+     * @param userId
+     * @return
+     */
+    @RequestMapping(path = "/restful/{userId}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<?> showUserInfoRest(@PathVariable("userId") Integer userId) {
+        User user = userDao.getById(userId);
+        HttpStatus httpStatus = HttpStatus.OK;
+        if (user == null) {
+            httpStatus = HttpStatus.NOT_FOUND;
+            Map<String, Object> error = new HashMap<>();
+            error.put("code", 404);
+            error.put("msg", "user with id :" + userId + " not found");
+            return new ResponseEntity<>(error, httpStatus);
+        }
+
+        return new ResponseEntity<>(user, httpStatus);
+    }
+
+    /**
+     * restful api
+     * consumes = "application/json" 表明这个方法只接受json数据，即发送请求时，声明Content-Type头部要么不设置（说明可以接受任意类型），要么为[Content-Type: application/json]，其他调用此方法将报错
+     * 标注@RequestBody 表明这个方法的参数是从http resopnse body转换而来的，并启用相应的Converter将body转换成相应的数据形式，如：json
+     * RequestEntity<T> 请求提参数，可以不用加标注@RequestBody
+     * <p/>
+     * 标注@ResponseBody 表明这个方法的返回值是http resopnse body，并启用相应的Converter将返回值转换成相应的数据形式，如：json
+     * 当返回ResponseEntity<T>的时候，可以不用标注@ResponseBody，功能同上，可以添加响应状态码，头部信息等等
+     *
+     * @param requestEntity
+     * @return
+     */
+    @RequestMapping(path = "/restful", method = RequestMethod.POST, consumes = "application/json")
+    @ResponseBody
+//    public ResponseEntity<?> showUserInfoRest(@RequestBody User user) {
+    public ResponseEntity<?> showUserInfoRest(RequestEntity<User> requestEntity) {
+        System.out.println("type: " + requestEntity.getType());//表示的是RequestEntity.getBody()类型，即：type: class com.magicli.ioc.domain.User
+        System.out.println("url: " + requestEntity.getUrl());//http://localhost:8080/user/restful
+        System.out.println("method: " + requestEntity.getMethod());//POST
+        User userSaved = userDao.save(requestEntity.getBody());
+        HttpStatus httpStatus = HttpStatus.CREATED;
+        if (userSaved == null || userSaved.getId() == null) {
+            httpStatus = HttpStatus.FORBIDDEN;
+            Map<String, Object> error = new HashMap<>();
+            error.put("code", 403);
+            error.put("msg", "user save FORBIDDEN");
+            return new ResponseEntity<>(error, httpStatus);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("http://localhost:8080/user/" + userSaved.getId()));
+
+        ResponseEntity responseEntity = new ResponseEntity<>(userSaved, headers, httpStatus);
+
+        return responseEntity;
+    }
+
+    /**
      * 也可以使用javax.servlet.http.Part
      * 如果使用javax.servlet.http.Part，就可以不用配置MultipartResolver了，方便快捷（不过只能在servlet3.0中使用）
      */
@@ -53,7 +122,7 @@ public class UserInfoController {
         System.out.println("userId: " + userId);
         System.out.println("title: " + title);
 
-        File dest = new File(baseDir,originalFilename);
+        File dest = new File(baseDir, originalFilename);
 
         try {
             profile.transferTo(dest);
@@ -62,8 +131,8 @@ public class UserInfoController {
         }
 
 
-        model.addAttribute("userId",userId);
-        model.addAttribute("title",title);
+        model.addAttribute("userId", userId);
+        model.addAttribute("title", title);
 
         //url重定向，spring会吧model里面的参数放到URL下面去（不要用字符串拼接的，用占位符）
         //假设userId=1 title='china' 如下的URL会重定向到  /user/1?title=china
